0c95f0fb1ac42419ecda8d35b666d81def4c4701
AsyaCemeniel <asyaorlov89@gmail.com>
2021-07-25 12:55:10 +0300

    Fix ingredient details

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -28,19 +28,27 @@ const IngredientDetails = () => {
           <div className={`text_color_inactive ${styles.nutrition}`}>
             <div className={`mr-5 ${styles.item}`}>
               <span className="text text_type_main-default">Калории,ккал</span>
-              <span className="text text_type_digits-default mt-4">244,4</span>
+              <span className="text text_type_digits-default mt-4">
+                {product.calories}
+              </span>
             </div>
             <div className={`mr-5 ${styles.item}`}>
               <span className="text text_type_main-default">Белки, г</span>
-              <span className="text text_type_digits-default mt-4">12,2</span>
+              <span className="text text_type_digits-default mt-4">
+                {product.proteins}
+              </span>
             </div>
             <div className={`mr-5 ${styles.item}`}>
               <span className="text text_type_main-default">Жиры, г</span>
-              <span className="text text_type_digits-default mt-4">17,2</span>
+              <span className="text text_type_digits-default mt-4">
+                {product.fat}
+              </span>
             </div>
             <div className={` ${styles.item}`}>
               <span className="text text_type_main-default">Углеводы, г</span>
-              <span className="text text_type_digits-default mt-4">10,2</span>
+              <span className="text text_type_digits-default mt-4">
+                {product.carbohydrates}
+              </span>
             </div>
           </div>
         </div>
